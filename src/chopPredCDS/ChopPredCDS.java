8d944e61b066da892169e4fb30e531916d251e49
Shohei <Shohei@10.242.255.51>
2018-06-17 17:09:08 +0900

    first

---
+++ src/chopPredCDS/ChopPredCDS.java
@@ -0,0 +1,26 @@
+package chopPredCDS;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ChopPredCDS {
+	
+	File inputFile, outputFile;
+	
+	public static void main(String args[]) {
+		
+	}
+	
+//========================Constructor=========================
+/*	public ChopPredCDS(File file) {
+		this.inputFile = file;
+	}*/
+	
+	private void process() {
+		
+	}
+	
+	private void prepareIO() throws IOException {
+		
+	}
+}
